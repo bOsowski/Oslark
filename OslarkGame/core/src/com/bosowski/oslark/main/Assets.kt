708d211e62f7ba3da3214b36efd9a8f389a9112d
Bartosz Osowski <bOsowski04@gmail.com>
2018-11-22 02:25:40 +0000

    Ported to kotlin.

---
+++ OslarkGame/core/src/com/bosowski/oslark/main/Assets.kt
@@ -0,0 +1,100 @@
+package com.bosowski.oslark.main
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.assets.AssetDescriptor
+import com.badlogic.gdx.assets.AssetErrorListener
+import com.badlogic.gdx.assets.AssetManager
+import com.badlogic.gdx.graphics.g2d.Animation
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.utils.Array
+import com.badlogic.gdx.utils.Disposable
+import com.bosowski.oslark.enums.State
+import com.bosowski.oslark.utils.Constants
+
+import java.util.HashMap
+
+/**
+ * Singleton class responsible for asset management.
+ */
+class Assets private constructor()//empty private constructor. -> prevents instantiating the class from outside.
+  : Disposable, AssetErrorListener {
+  private var assetManager: AssetManager? = null
+  val textures = HashMap<String, TextureRegion>()
+  val animations = HashMap<String, Animation<TextureRegion>>()
+  val stateAnimations = HashMap<String, HashMap<State, Animation<TextureRegion>>>()
+
+  fun init(assetManager: AssetManager) {
+    this.assetManager = assetManager
+    assetManager.setErrorListener(this)
+
+    //load texture from game sprites
+    assetManager.load(Constants.TEXTURE_ATLAS, TextureAtlas::class.java)
+    assetManager.finishLoading()
+
+    for (name in assetManager.assetNames) {
+      Gdx.app.debug(TAG, "asset '$name' loaded.")
+    }
+
+    //create atlas for game sprites
+    val atlas = assetManager.get<TextureAtlas>(Constants.TEXTURE_ATLAS)
+
+    for (region in atlas.regions) {
+      //insert the loaded texture into the map.
+      if (region.index == -1) {
+        textures[region.name] = region
+        Gdx.app.debug(TAG, "texture: '" + region.name + "' loaded.")
+      } else if (!animations.containsKey(region.name)) {
+        val animation = atlas.findRegions(region.name)
+        animations[region.name] = Animation<TextureRegion>(Constants.FRAME_DURATION, animation, Animation.PlayMode.LOOP)
+        Gdx.app.debug(TAG, "animation: '" + region.name + ", amount of animations: " + animation.size + "' loaded.")
+      }
+
+    }
+
+    for (animationName in animations.keys) {
+      println(animationName)
+      if (animationName.contains("_")) {
+        if (animationName.contains("male") || animationName.contains("female")) {
+          val split = animationName.split("_".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+          if (!stateAnimations.containsKey(split[0] + split[1])) {
+            stateAnimations[split[0] + split[1]] = HashMap<State, Animation<TextureRegion>>()
+          }
+          if (!stateAnimations[split[0] + split[1]]!!.containsKey(State.getState(split[2]))) {
+            stateAnimations[split[0] + split[1]]!![State.getState(split[2])] = animations[animationName]!!
+          }
+          stateAnimations[split[0] + split[1]]!![State.getState(split[2])] = animations[animationName]!!
+          println(split[0] + split[1])
+
+        } else {
+          val split = animationName.split("_".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+          if (!stateAnimations.containsKey(split[0])) {
+            stateAnimations[split[0]] = HashMap()
+          }
+          if (!stateAnimations[split[0]]!!.containsKey(State.getState(split[1]))) {
+            stateAnimations[split[0]]!![State.getState(split[1])] = animations[animationName]!!
+          }
+
+          stateAnimations[split[0]]!![State.getState(split[1])] = animations[animationName]!!
+        }
+
+      }
+    }
+  }
+
+  override fun error(asset: AssetDescriptor<*>, throwable: Throwable) {
+    Gdx.app.error(TAG, "Couldn't load asset '$asset'", throwable)
+  }
+
+  override fun dispose() {
+    assetManager!!.dispose()
+  }
+
+  companion object {
+
+    val instance = Assets()
+    val TAG = Assets::class.java.name
+  }
+
+
+}
---
+++ OslarkGame/core/src/com/bosowski/oslark/screens/CharacterSelectionScreen.kt
@@ -0,0 +1,105 @@
+package com.bosowski.oslark.screens
+
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.bosowski.oslark.World
+import com.bosowski.oslark.gameObjects.Player
+import com.bosowski.oslark.main.Session
+
+import org.json.JSONArray
+import org.json.JSONObject
+
+import java.io.IOException
+import java.util.LinkedHashMap
+
+class CharacterSelectionScreen(game: Game) : AbstractGameScreen(game) {
+
+  private val characterArray: JSONArray
+
+  init {
+    var userJson: String? = null
+    try {
+      userJson = Session.instance.loadUser()
+    } catch (e: IOException) {
+      e.printStackTrace()
+    }
+
+    println(userJson)
+    val obj = JSONObject(userJson)
+    println(obj.get("username"))
+    characterArray = obj.getJSONArray("characters")
+    setUpUI()
+  }
+
+  override fun setUpUI() {
+    val label = Label("Choose your character", fieldSkins)
+    label.setPosition((Gdx.graphics.width / 3).toFloat(), (Gdx.graphics.height - 100).toFloat())
+    stage.addActor(label)
+
+    val characters = LinkedHashMap<String, Player>()
+
+    var i = 0
+    for (character in characterArray) {
+      val player = Player(character as JSONObject)
+      val characterButton = TextButton(player.name + "(level " + player.level + " " + player.characterClass, fieldSkins)
+      characters[player.name] = player
+      characterButton.setPosition(label.x, label.y - 50 * (i + 1))
+      characterButton.addListener(object : ClickListener() {
+        override fun touchDown(event: InputEvent?, x: Float, y: Float, point: Int, button: Int): Boolean {
+          println("Picked " + player.name)
+          World.instance.setPlayer(characters[player.name]!!)
+          //                    String worldJson = null;
+          //                    try {
+          //                        worldJson = Session.instance.loadWorld("characterName=" + World.instance.getPlayer().getName());
+          //
+          //                    } catch (IOException e) {
+          //                        e.printStackTrace();
+          //                    }
+          //                    System.out.println(worldJson);
+          //                    JSONObject jsonObj = new JSONObject(worldJson);
+          //                    JSONArray terrain = jsonObj.getJSONArray("terrain");
+          //                    for (Object tile : terrain) {
+          //                        Terrain terrainTile = new Terrain((JSONObject) tile);
+          //                        World.instance.instantiate(terrainTile);
+          //                        System.out.println("Loaded terrain: " + terrainTile);
+          //                    }
+
+          game.screen = GameScreen(game)
+          return true
+        }
+      })
+      stage.addActor(characterButton)
+      i++
+    }
+  }
+
+  override fun render(deltaTime: Float) {
+    // Clear the buffer
+    Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+    stage.act(deltaTime)
+    stage.draw()
+  }
+
+  override fun resize(width: Int, height: Int) {
+    super.resize(width, height)
+  }
+
+  override fun show() {
+    Gdx.input.inputProcessor = stage
+  }
+
+  override fun hide() {
+
+  }
+
+  override fun pause() {
+
+  }
+}
